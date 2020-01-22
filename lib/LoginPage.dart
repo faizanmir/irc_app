f7d30dc6d17b525c42dc101f42325a84fef9aae2
Faizan <faizanmir009@gmail.com>
2020-01-22 12:20:02 +0530

    Dev

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
+abstract class ControlInterface {
+  _changeForLoading();
+  _errorOccurred();
+}
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> implements ControlInterface {
+  double _height = 450;
+  double _width = 450;
+  bool _isLoading = false;
+  final _emailTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+
+
   @override
   Widget build(BuildContext context) {
+    print(_isLoading);
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -17,9 +32,11 @@ class _LoginPageState extends State<LoginPage> {
           ], begin: Alignment.center, end: Alignment(2.0, 1.5)),
         ),
         child: Center(
-          child: Container(
-              height: 450,
-              width: 400,
+          child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              curve: Curves.easeOutCubic,
+              height: _height,
+              width: _width,
               margin: EdgeInsets.all(10),
               child: Card(
                 color: Color.fromARGB(255, 253, 253, 253),
@@ -45,18 +62,25 @@ class _LoginPageState extends State<LoginPage> {
                           SizedBox(
                             height: 20,
                           ),
-                          UserFormFields(
-                              "Email",
-                              Icon(
-                                Icons.email,
-                                color: Color.fromARGB(255, 149, 208, 158),
-                              ),
-                              false,
-                              1),
-                          SizedBox(
-                            height: 40,
-                          ),
-                          UserFormFields("Password", Icon(Icons.lock), true, 0)
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              children: <Widget>[
+                                UserFormFields(
+                                    "E-mail",
+                                    Icon(
+                                      Icons.email,
+                                      color: Color.fromARGB(255, 149, 208, 158),
+                                    ),
+                                    false,
+                                    1,
+                                    _emailTextController,
+                                    this),
+                                UserFormFields("Password", Icon(Icons.lock),
+                                    true, 0, _passwordTextController, this)
+                              ],
+                            ),
+                          )
                         ],
                       ),
                     ),
@@ -65,24 +89,13 @@ class _LoginPageState extends State<LoginPage> {
                         alignment: Alignment.bottomCenter,
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 150,
-                            height: 60,
-                            child: Card(
-                              elevation: 10,
-                              color: Color.fromARGB(255, 149, 208, 158),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30)),
-                              child: Center(
-                                  child: Text(
-                                "Login",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w300,
-                                    fontSize: 20),
-                              )),
-                            ),
-                          ),
+                          child: (_isLoading)
+                              ? LoginLoader()
+                              : LoginButton(
+                                  _isLoading,
+                                  _height,
+                                  this,
+                                ),
                         ),
                       ),
                     )
@@ -93,6 +106,101 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
+
+  @override
+  _changeForLoading() {
+    setState(() {
+      _height = 425;
+      _isLoading = true;
+      if(_formKey.currentState.validate())
+        {
+         _email  =  _emailTextController.text;
+         _password =  _passwordTextController.text;
+         doFirebaseLogin(_email, _password);
+        }
+    });
+
+
+
+
+  }
+
+
+  doFirebaseLogin(String email,String password){
+    Future.delayed(Duration(seconds: 5)).then((value) {
+      setState(() {
+        _height =450;
+        _isLoading = false;
+      });
+    });
+
+  }
+
+  @override
+  _errorOccurred() {
+    setState(() {
+      _height = 450;
+      _isLoading = false;
+    });
+  }
+}
+
+class LoginLoader extends StatefulWidget {
+  @override
+  _LoginLoaderState createState() => _LoginLoaderState();
+}
+
+class _LoginLoaderState extends State<LoginLoader> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPadding(
+      duration: Duration(seconds: 1),
+      padding: EdgeInsets.all(20),
+      child:
+          SizedBox(height: 30, width: 30, child: CircularProgressIndicator()),
+    );
+  }
+}
+
+class LoginButton extends StatefulWidget {
+  bool isLoading = false;
+  double height;
+  ControlInterface mListener;
+  LoginButton(this.isLoading, this.height, this.mListener);
+
+  @override
+  _LoginButtonState createState() => _LoginButtonState();
+}
+
+class _LoginButtonState extends State<LoginButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 60,
+      child: Card(
+        elevation: 10,
+        color: Color.fromARGB(255, 149, 208, 158),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        child: GestureDetector(
+          onTap: () {
+            widget.mListener._changeForLoading();
+          },
+          child: Center(
+              child: Text(
+            "Login",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w300, fontSize: 20),
+          )),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 }
 
 class UserFormFields extends StatefulWidget {
@@ -100,7 +208,10 @@ class UserFormFields extends StatefulWidget {
   final Icon icon;
   final bool isObscure;
   final int detect;
-  UserFormFields(this.text, this.icon, this.isObscure, this.detect);
+  final controller;
+  final ControlInterface mListener;
+  UserFormFields(this.text, this.icon, this.isObscure, this.detect,
+      this.controller, this.mListener);
 
   @override
   _UserFormFieldsState createState() =>
@@ -111,33 +222,57 @@ class _UserFormFieldsState extends State<UserFormFields> {
   final String text;
   final Icon icon;
   bool isObscure;
-  int detect;
-  _UserFormFieldsState(this.text, this.icon, this.isObscure, this.detect);
+  int detectEmailOrPassword;// 0 for password 1 for email
+  _UserFormFieldsState(this.text, this.icon, this.isObscure, this.detectEmailOrPassword);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      obscureText: isObscure,
-      decoration: InputDecoration(
-        labelText: text,
-        focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: Color.fromARGB(255, 149, 208, 158))),
-        enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: Color.fromARGB(255, 149, 208, 158))),
-        prefixIcon: icon,
-        suffixIcon: IconButton(
-          icon: Icon(
-            (detect == 0) ? Icons.remove_red_eye : null,
-            color:
-                (isObscure) ? Color.fromARGB(255, 149, 208, 158) : Colors.blue,
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: TextFormField(
+        validator: (value) {
+          if (value.isEmpty && detectEmailOrPassword == 0) {
+            widget.mListener._errorOccurred();
+            return "Please enter a password ";
+          } else if (!value.contains("@") && detectEmailOrPassword == 1) {
+            widget.mListener._errorOccurred();
+            return "Invalid Email address";
+          } else if (value.length < 6 && detectEmailOrPassword == 0) {
+            widget.mListener._errorOccurred();
+            return "Password should be greater than 6 characters";
+          }
+          return null;
+        },
+        controller: widget.controller,
+        obscureText: isObscure,
+        decoration: InputDecoration(
+          labelText: text,
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide:
+                  BorderSide(color: Color.fromARGB(255, 149, 208, 158))),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide:
+                  BorderSide(color: Color.fromARGB(255, 149, 208, 158))),
+          errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide:
+                  BorderSide(color: Color.fromARGB(255, 149, 208, 158))),
+          prefixIcon: icon,
+          suffixIcon: IconButton(
+            icon: Icon(
+              (detectEmailOrPassword == 0) ? Icons.remove_red_eye : null,
+              color: (isObscure)
+                  ? Color.fromARGB(255, 149, 208, 158)
+                  : Colors.blue,
+            ),
+            onPressed: () {
+              setState(() {
+                isObscure = !isObscure;
+              });
+            },
           ),
-          onPressed: () {
-            setState(() {
-              isObscure = !isObscure;
-            });
-          },
         ),
       ),
     );
